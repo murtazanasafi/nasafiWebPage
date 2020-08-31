e7291de3376a92a9137b8d79343fe9de840c5e5d
murtazanasafi <nasafi.murtaza@gmail.com>
2020-08-31 16:58:26 -0700

    Version v 0.1 update

--- nasafiwebpage/settings.py
+++ nasafiwebpage/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from secret_key import SECRET_KEY_key
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
